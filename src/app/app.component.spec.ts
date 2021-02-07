45e4785f7440ba08accbcb184ab6f68c8b0e3b89
Daniel Silva Moreira <daniel.smoreira@outlook.com>
2021-02-06 23:24:13 -0300

    Remoção do teste e2e, do karma e adição/configuração do jest e do husky

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { ComponentFixture } from '@angular/core/testing';
+import { render, RenderResult } from '@testing-library/angular';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let renderResult: RenderResult<AppComponent>;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+    renderResult = await render(AppComponent, {
+      imports: [BrowserModule, RouterTestingModule],
+    });
+    fixture = renderResult.fixture;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'rental-cars'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('rental-cars');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('rental-cars app is running!');
+  it('deve ser criado', () => {
+    const { container } = renderResult;
+    expect(container).toBeInTheDocument();
   });
 });
